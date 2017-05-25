2888e2c7797e1e776de1fc1fc94ca197c80ad236
Will Hansen <whansen@millenniumhealth.com>
2017-05-25 10:10:32 -0700

    correlated print function

--- app/modules/correlatedPrint.js
+++ app/modules/correlatedPrint.js
@@ -1,16 +1,48 @@
 var excel = require('./excel');
 var print = require('./print');
+var fs = require('fs');
 
 // console.log(excel.excelToJSON('C:\\Users\\whansen\\Desktop\\test1.xlsx'))
 // var data = "ABC";
 // console.log("0xF1" + data.charCodeAt(2).toString(16));
 
-var stringToHex = function(str){
-    var arr = [];
-    for (i=0; i<str.length; i++){
-        arr.push(str.charCodeAt(i).toString(16));
-    }
-    return arr.toString().replace(/,/g , "");
+
+// print.printWithOptions('C:\\Users\\whansen\\Desktop\\Provider Inquiry Form.pdf', 'http://10.1.205.71','claim')
+
+var correlatedPrint = function(xlFile, medrecLoc, claimLoc, uri){
+    var workObj = excel.excelToJSON(xlFile);
+    // console.log(workObj);
+    // workObj.forEach(function(entry, index){
+    for (i=0; i<workObj.length; i++){
+        var Accn = workObj[i]['Accn ID'];
+        console.log('Working on ' + Accn + ' (' +  (i+1) + ' of ' + workObj.length + ')');
+        var medRec = findFile(medrecLoc, Accn);
+        var claim = findFile(claimLoc, Accn);
+        if (!medRec){
+            console.log('Unable to locate Medical Records');
+            continue;
+        }
+        if (!claim){
+            console.log('Unable to locate Claim');
+            continue;
+        }
+        var medRecFilePath = medrecLoc + '\\' + medRec;
+        var claimFilePath = claimLoc + '\\' + claim;
+        print.printWithOptions(claimFilePath, uri, 'claim');
+        print.printWithOptions(medRecFilePath, uri, 'medrec');
+        console.log('sent')
+    };
 }
 
-console.log(stringToHex("http://10.1.205.71"));
+var findFile = function(searchLoc, Accn){
+    var files = fs.readdirSync(searchLoc);
+    for (i=0; i<files.length; i++){
+        if ( files[i].toLowerCase().indexOf(Accn.toLowerCase()) >= 0 ){
+            return files[i]
+        };
+    };
+    return 0
+};
+
+correlatedPrint('c:\\users\\whansen\\desktop\\test1.xlsx', 'c:\\users\\whansen\\desktop\\2017-05-25-0921-whansen', 'C:\\Users\\whansen\\Desktop\\pdfParser\\work\\New folder', 'http://10.1.205.71');
+// console.log(findFile('c:\\users\\whansen\\desktop\\2017-05-16-1253-whansen', 'AB1382963'));
--- app/modules/print.js
+++ app/modules/print.js
@@ -2,23 +2,28 @@ var PDFDocument = require("pdfkit");
 var path = require('path');
 let fs = require('fs');
 var ipp = require('ipp');
-var uri = "http://10.1.205.71";
+// var uri = "http://10.1.205.71";
+// claims print to tray 2 everything else to tray 1
 
 module.exports = {
-  printWithOptions: function(file, uri, tray, fit){
+  printWithOptions: function(file, uri, type){
+    console.log("Printing " + file + " as " + type);
     file = path.normalize(file);
     var readStream = fs.createReadStream(file);
 
     var buffers = [];
     readStream.on('data', buffers.push.bind(buffers));
     readStream.on('end', function(){
+      var msg = createMsg(uri, type);
       var buf = Buffer.concat(buffers);
-      console.log(buf);
+      // console.log(ipp.parse(msg));
       var catBuf = Buffer.concat([msg, buf]);
+      console.log(file);
       ipp.request(uri, catBuf, function(err, res){
         if(err){
-          return console.log(err);
+          return console.log("Error Printing!");
         }
+        console.log("Success Printing!");
         console.log(JSON.stringify(res,null,2));
       });
     });
@@ -65,60 +70,6 @@ module.exports = {
 //     '37'+ //end of collection
 //     '00000000'+ //name length and value length
 //   '03', 'hex');
-
-
-//need to add fittopage
-  var createMsg = function(uri, tray){
-     var msg = 
-    '0200'+ //Version
-    '000201e6d5f2'+
-    '01'+ //Operation attributes tag
-      '47'+ //charset tag
-      '0012'+ //length
-      '617474726962757465732d63686172736574'+ //attributes-charset
-      '0005'+ //length
-      '7574662d38'+ //utf-8
-      '48'+ //natural language tag
-      '001b'+ //length
-      '617474726962757465732d6e61747572616c2d6c616e6775616765'+//attributes-natural-language
-      '0002'+//length
-      '656e'+ //en
-      '45'+ // URI tag
-      '000b'+ //length
-      '7072696e7465722d757269'+ //printer-uri
-      '0012'+//length
-      stringToHex(uri)+
-      '49'+ //mimeMediaType tag
-      '000f'+ //length
-      '646f63756d656e742d666f726d6174'+ //document format
-      '000f'+ //length
-      '6170706c69636174696f6e2f706466'+ //application/pdf
-    '02'+ //job attributes tag
-      '34'+ //begin collection
-        '0009'+ //length
-        '6d656469612d636f6c'+ //media-col
-        '0000'+ //value length
-        '4a'+ //collection entry
-        '0000'+ //name length
-        '000c'+ //value length
-        '6d656469612d736f75726365'+ //media-source
-        '44'+ // collection entry
-        '0000'+ //name length
-        '0006'+ //value length
-        stringToHex(tray)+
-        '37'+ //end of collection
-      '00000000'+ //name length and value length
-    '03';
-    var msgbuf = new Buffer(msg, 'hex');
-    return msgbuf;
-  }
-
-var noScale = '44'+//keyword tag
-  ''+//length
-  ''+ //name
-  ''+//length
-  '';//value
-
 var stringToHex = function(str){
     var arr = [];
     for (i=0; i<str.length; i++){
@@ -126,6 +77,297 @@ var stringToHex = function(str){
     }
     return arr.toString().replace(/,/g , "");
 }
+
+var createMsg = function(uri, type){
+  if ( type === 'claim'){
+    var claimMsg = new Buffer(
+      '0200'+ //Version
+      '000201e6d5f2'+
+      '01'+ //Operation attributes tag
+        '47'+ //charset tag
+        '0012'+ //length
+        '617474726962757465732d63686172736574'+ //attributes-charset
+        '0005'+ //length
+        '7574662d38'+ //utf-8
+        '48'+ //natural language tag
+        '001b'+ //length
+        '617474726962757465732d6e61747572616c2d6c616e6775616765'+//attributes-natural-language
+        '0002'+//length
+        '656e'+ //en
+        '45'+ // URI tag
+        '000b'+ //length
+        '7072696e7465722d757269'+ //printer-uri
+        '0012'+//length
+        stringToHex(uri)+
+        '49'+ //mimeMediaType tag
+        '000f'+ //length
+        '646f63756d656e742d666f726d6174'+ //document format
+        '000f'+ //length
+        '6170706c69636174696f6e2f706466'+ //application/pdf
+      '02'+ //job attributes tag
+        '34'+ //begin collection
+          '0009'+ //length
+          '6d656469612d636f6c'+ //media-col
+          '0000'+ //value length
+          '4a'+ //collection entry
+          '0000'+ //name length
+          '000c'+ //value length
+          '6d656469612d736f75726365'+ //media-source
+          '44'+ // collection entry
+          '0000'+ //name length
+          '0006'+ //value length
+          '747261792d32'+ //tray-2
+        '37'+ //end of collection
+        '00000000'+ //name length and value length
+        '44'+
+        '000d'+
+        '7072696e742d7363616c696e67'+
+        '0004'+
+        '6e6f6e65'+
+      '03', 'hex');
+      return claimMsg;
+  } else {
+    var medRecMsg = new Buffer(
+      '0200'+ //Version
+      '000201e6d5f2'+
+      '01'+ //Operation attributes tag
+        '47'+ //charset tag
+        '0012'+ //length
+        '617474726962757465732d63686172736574'+ //attributes-charset
+        '0005'+ //length
+        '7574662d38'+ //utf-8
+        '48'+ //natural language tag
+        '001b'+ //length
+        '617474726962757465732d6e61747572616c2d6c616e6775616765'+//attributes-natural-language
+        '0002'+//length
+        '656e'+ //en
+        '45'+ // URI tag
+        '000b'+ //length
+        '7072696e7465722d757269'+ //printer-uri
+        '0012'+//length
+        stringToHex(uri)+
+        '49'+ //mimeMediaType tag
+        '000f'+ //length
+        '646f63756d656e742d666f726d6174'+ //document format
+        '000f'+ //length
+        '6170706c69636174696f6e2f706466'+ //application/pdf
+      '02'+ //job attributes tag
+        '34'+ //begin collection
+          '0009'+ //length
+          '6d656469612d636f6c'+ //media-col
+          '0000'+ //value length
+          '4a'+ //collection entry
+          '0000'+ //name length
+          '000c'+ //value length
+          '6d656469612d736f75726365'+ //media-source
+          '44'+ // collection entry
+          '0000'+ //name length
+          '0006'+ //value length
+          '747261792d31'+ //tray-1
+        '37'+ //end of collection
+        '00000000'+ //name length and value length
+      '03', 'hex');
+      return medRecMsg;
+  };
+}
+
+// var claimMsg = new Buffer(
+//   '0200'+ //Version
+//   '000201e6d5f2'+
+//   '01'+ //Operation attributes tag
+//     '47'+ //charset tag
+//     '0012'+ //length
+//     '617474726962757465732d63686172736574'+ //attributes-charset
+//     '0005'+ //length
+//     '7574662d38'+ //utf-8
+//     '48'+ //natural language tag
+//     '001b'+ //length
+//     '617474726962757465732d6e61747572616c2d6c616e6775616765'+//attributes-natural-language
+//     '0002'+//length
+//     '656e'+ //en
+//     '45'+ // URI tag
+//     '000b'+ //length
+//     '7072696e7465722d757269'+ //printer-uri
+//     '0012'+//length
+//     '687474703a2f2f31302e312e3230352e3731'+//http://10.1.205.71
+//     '49'+ //mimeMediaType tag
+//     '000f'+ //length
+//     '646f63756d656e742d666f726d6174'+ //document format
+//     '000f'+ //length
+//     '6170706c69636174696f6e2f706466'+ //application/pdf
+//   '02'+ //job attributes tag
+//     '34'+ //begin collection
+//       '0009'+ //length
+//       '6d656469612d636f6c'+ //media-col
+//       '0000'+ //value length
+//       '4a'+ //collection entry
+//       '0000'+ //name length
+//       '000c'+ //value length
+//       '6d656469612d736f75726365'+ //media-source
+//       '44'+ // collection entry
+//       '0000'+ //name length
+//       '0006'+ //value length
+//       '747261792d32'+ //tray-2
+//     '37'+ //end of collection
+//     '00000000'+ //name length and value length
+//     '44'+
+//     '000d'+
+//     '7072696e742d7363616c696e67'+
+//     '0004'+
+//     '6e6f6e65'+
+//   '03', 'hex');
+
+// var medRecMsg = new Buffer(
+//   '0200'+ //Version
+//   '000201e6d5f2'+
+//   '01'+ //Operation attributes tag
+//     '47'+ //charset tag
+//     '0012'+ //length
+//     '617474726962757465732d63686172736574'+ //attributes-charset
+//     '0005'+ //length
+//     '7574662d38'+ //utf-8
+//     '48'+ //natural language tag
+//     '001b'+ //length
+//     '617474726962757465732d6e61747572616c2d6c616e6775616765'+//attributes-natural-language
+//     '0002'+//length
+//     '656e'+ //en
+//     '45'+ // URI tag
+//     '000b'+ //length
+//     '7072696e7465722d757269'+ //printer-uri
+//     '0012'+//length
+//     '687474703a2f2f31302e312e3230352e3731'+//http://10.1.205.71
+//     '49'+ //mimeMediaType tag
+//     '000f'+ //length
+//     '646f63756d656e742d666f726d6174'+ //document format
+//     '000f'+ //length
+//     '6170706c69636174696f6e2f706466'+ //application/pdf
+//   '03', 'hex');
+
+// var claimMsg = new Buffer(
+//   '0200'+ //Version
+//   '000201e6d5f2'+
+//   '01'+ //Operation attributes tag
+//     '47'+ //charset tag
+//     '0012'+ //length
+//     '617474726962757465732d63686172736574'+ //attributes-charset
+//     '0005'+ //length
+//     '7574662d38'+ //utf-8
+//     '48'+ //natural language tag
+//     '001b'+ //length
+//     '617474726962757465732d6e61747572616c2d6c616e6775616765'+//attributes-natural-language
+//     '0002'+//length
+//     '656e'+ //en
+//     '45'+ // URI tag
+//     '000b'+ //length
+//     '7072696e7465722d757269'+ //printer-uri
+//     '0012'+//length
+//     '687474703a2f2f31302e312e3230352e3731'+//http://10.1.205.71
+//     '49'+ //mimeMediaType tag
+//     '000f'+ //length
+//     '646f63756d656e742d666f726d6174'+ //document format
+//     '000f'+ //length
+//     '6170706c69636174696f6e2f706466'+ //application/pdf
+//   '02'+ //job attributes tag
+//     '34'+ //begin collection
+//       '0009'+ //length
+//       '6d656469612d636f6c'+ //media-col
+//       '0000'+ //value length
+//       '4a'+ //collection entry
+//       '0000'+ //name length
+//       '000c'+ //value length
+//       '6d656469612d736f75726365'+ //media-source
+//       '44'+ // collection entry
+//       '0000'+ //name length
+//       '0006'+ //value length
+//       '747261792d32'+ //tray-2
+//     '37'+ //end of collection
+//     '00000000'+ //name length and value length
+//     '44'+
+//     '000d'+
+//     '7072696e742d7363616c696e67'+
+//     '0004'+
+//     '6e6f6e65'+
+//   '03', 'hex');
+
+// var medRecMsg = new Buffer(
+//   '0200'+ //Version
+//   '000201e6d5f2'+
+//   '01'+ //Operation attributes tag
+//     '47'+ //charset tag
+//     '0012'+ //length
+//     '617474726962757465732d63686172736574'+ //attributes-charset
+//     '0005'+ //length
+//     '7574662d38'+ //utf-8
+//     '48'+ //natural language tag
+//     '001b'+ //length
+//     '617474726962757465732d6e61747572616c2d6c616e6775616765'+//attributes-natural-language
+//     '0002'+//length
+//     '656e'+ //en
+//     '45'+ // URI tag
+//     '000b'+ //length
+//     '7072696e7465722d757269'+ //printer-uri
+//     '0012'+//length
+//     '687474703a2f2f31302e312e3230352e3731'+//http://10.1.205.71
+//     '49'+ //mimeMediaType tag
+//     '000f'+ //length
+//     '646f63756d656e742d666f726d6174'+ //document format
+//     '000f'+ //length
+//     '6170706c69636174696f6e2f706466'+ //application/pdf
+//   '03', 'hex');
+
+
+// //need to add fittopage
+//   var createMsg = function(uri, tray){
+//      var msg = 
+//     '0200'+ //Version
+//     '000201e6d5f2'+
+//     '01'+ //Operation attributes tag
+//       '47'+ //charset tag
+//       '0012'+ //length
+//       '617474726962757465732d63686172736574'+ //attributes-charset
+//       '0005'+ //length
+//       '7574662d38'+ //utf-8
+//       '48'+ //natural language tag
+//       '001b'+ //length
+//       '617474726962757465732d6e61747572616c2d6c616e6775616765'+//attributes-natural-language
+//       '0002'+//length
+//       '656e'+ //en
+//       '45'+ // URI tag
+//       '000b'+ //length
+//       '7072696e7465722d757269'+ //printer-uri
+//       '0012'+//length
+//       stringToHex(uri)+
+//       '49'+ //mimeMediaType tag
+//       '000f'+ //length
+//       '646f63756d656e742d666f726d6174'+ //document format
+//       '000f'+ //length
+//       '6170706c69636174696f6e2f706466'+ //application/pdf
+//     '02'+ //job attributes tag
+//       '34'+ //begin collection
+//         '0009'+ //length
+//         '6d656469612d636f6c'+ //media-col
+//         '0000'+ //value length
+//         '4a'+ //collection entry
+//         '0000'+ //name length
+//         '000c'+ //value length
+//         '6d656469612d736f75726365'+ //media-source
+//         '44'+ // collection entry
+//         '0000'+ //name length
+//         '0006'+ //value length
+//         stringToHex(tray)+
+//       '37'+ //end of collection
+//       '00000000'+ //name length and value length
+//     '03';
+//     var msgbuf = new Buffer(msg, 'hex');
+//     return msgbuf;
+//   }
+
+// var noScale = '44'+//keyword tag
+//   ''+//length
+//   ''+ //name
+//   ''+//length
+//   '';//value
+
 
 // readStream.end();
 // var doc = new PDFDocument;
